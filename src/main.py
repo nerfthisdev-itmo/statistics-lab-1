c72ada6e4d0d318ee37f290214719cc53bf31ff6
nerfthisdev <353054@niuitmo.ru>
2025-02-13 21:25:46 +0300

    feat: add main

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from stats.variation import *
+from stats.dataoutput import *
+
+def main():
+    data = [5, 1, 3, 2, 4, 3, 3, 5, 6]
+    statistical = get_statistical_series(data)
+    
+
+if __name__ == "__main__":
+    main()
