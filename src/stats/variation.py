15021052af4ac8887fa2052757ed7ae83b1b0da1
nerfthisdev <353054@niuitmo.ru>
2025-02-13 21:25:16 +0300

    feat: add stats package with variation functions

---
+++ src/stats/variation.py
@@ -0,0 +1,21 @@
+
+
+def get_variation_series(data):
+    return sorted(data)
+
+def get_statistical_series(data):
+    number_freq = dict()
+    for num in data:
+        if (number_freq.get(num, None) == None):
+            number_freq[num] = 0
+        number_freq[num] += 1
+    return number_freq
+
+
+def get_extremes(nums: list):
+    sorted_nums: list = sorted(nums)
+
+    output_str: str = f"min: {sorted_nums[0]} "
+    output_str += f"max: {sorted_nums[-1]} "
+
+    return output_str
