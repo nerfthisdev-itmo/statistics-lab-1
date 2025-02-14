2055a0978fbf2059d0d09270bdf500b11a1bd529
nerfthisdev <353054@niuitmo.ru>
2025-02-14 14:30:27 +0300

    feat: make variation a class

--- src/stats/variation.py
+++ src/stats/variation.py
@@ -1,29 +1,38 @@
+class Variation:    
+    def __init__(self, data):
+        self.data = data
+        self.statistical_series = self.get_statistical_series()
+        self.mode = self.get_mode()
+        self.extremes = self.get_extremes()
+        
+        
+        
+    
+    def get_variation_series(self):
+        return sorted(self.data)
 
 
-def get_variation_series(data):
-    return sorted(data)
+    def get_statistical_series(self):
+        data = self.data
+        number_freq = dict()
+        for num in data:
+            if (number_freq.get(num, None) == None):
+                number_freq[num] = 0
+            number_freq[num] += 1
+        return number_freq
 
 
-def get_statistical_series(data):
-    number_freq = dict()
-    for num in data:
-        if (number_freq.get(num, None) == None):
-            number_freq[num] = 0
-        number_freq[num] += 1
-    return number_freq
+    def get_mode(self):
+        return max(self.statistical_series, key=self.statistical_series.get)
 
 
-def get_mode(nums:dict):
-    return max(nums, key=nums.get)
+    def get_extremes(self):
+        sorted_nums: list = sorted(self.data)
 
+        output_str: str = f"min: {sorted_nums[0]} "
+        output_str += f"max: {sorted_nums[-1]} "
 
-def get_extremes(nums: list):
-    sorted_nums: list = sorted(nums)
-
-    output_str: str = f"min: {sorted_nums[0]} "
-    output_str += f"max: {sorted_nums[-1]} "
-
-    return output_str
+        return output_str
 
 '''
 TODO: calculate: mode, range, expected value, standard diviation, CDF
