81b2d0d6f8dcc028453a2fab6b7a146d79433609
nerfthisdev <353054@niuitmo.ru>
2025-02-14 16:20:00 +0300

    feat: add interval series class

--- src/stats/variation.py
+++ src/stats/variation.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Variation:    
     def __init__(self, data):
         self.data = data
@@ -28,13 +30,34 @@ class Variation:
 
     def get_extremes(self):
         sorted_nums: list = sorted(self.data)
+        
+        self.max = sorted_nums[-1]
+        self.min = sorted_nums[0]
 
         output_str: str = f"min: {sorted_nums[0]} "
         output_str += f"max: {sorted_nums[-1]} "
 
         return output_str
 
-'''
-TODO: calculate: mode, range, expected value, standard diviation, CDF
 
 '''
+TODO: calculate: standard diviation, CDF
+'''
+
+class IntervalVariationSeries:
+    def __init__(self, data):
+        self.data = np.array(data)
+        self.min = np.min(self.data)
+        self.max = np.max(self.data)
+        self.range = self.max - self.min
+        self.n = len(self.data)
+        self.k = int(1 + np.log2(self.n))
+        self.h = self.range / self.k
+        self.bins = np.arange(self.min, self.max + self.h, self.h)
+    
+    def get_intervals(self):
+        return [(self.bins[i], self.bins[i+1]) for i in range(len(self.bins) - 1)]
+    
+    def get_frequencies(self):
+        hist, _ = np.histogram(self.data, bins=self.bins)
+        return hist
