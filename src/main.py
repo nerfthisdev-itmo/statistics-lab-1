cc3ca8d043c49d34bc7363b7a04aaf5e8b083ead
nerfthisdev <353054@niuitmo.ru>
2025-04-09 15:52:24 +0300

    complete task a output

--- src/main.py
+++ src/main.py
@@ -14,20 +14,35 @@ def load_data(file_path):
 
     
     
-def print_answer_A(x:Variation, presicion):
+def print_answer_A(x:Variation, precision):
+    dir_name = "output/task1/"
+    plot_cdf(x, dir_name)
+    plot_polygon(x, dir_name)
+    plot_cumulative(x, dir_name)
+    print(f"Эмпирическая функция распределения записана в {dir_name}eCDF.pdf")
+    print(f"Полигон частот записан в {dir_name}polygon.pdf")
+    print(f"Кумулята записана в {dir_name}cumulative.pdf \n")
+    
     print(f'n = {len(x.data)}')
     print_variation_series(x.statistical_series)
+    print()
     print(f'min: {x.min} max: {x.max}')
     print(f'Размах: {x.range}')
-    print(f'Оценка математического ожидания (начальный момент): {round(x.expected_value_estimate, presicion)}')
-    print(f'Стандартное отклонение: {round(x.expected_value_deviation, presicion)}')
-    print(f'Дисперсия: {round(x.sample_variance, presicion)}')
-    print(f'Оценка выборочного среднеквадратического отклонения: {round(x.sample_standard_deviation, presicion)}')
-    print(f'Оценка выборочного с.к.о. (исправленная): {round(x.sample_standard_deviation_corrected, presicion)}')
+    print(f'Оценка математического ожидания (начальный момент): {round(x.expected_value_estimate, precision)}')
+    print(f'Стандартное отклонение: {round(x.expected_value_deviation, precision)}')
+    print(f'Дисперсия: {round(x.sample_variance, precision)}')
+    print(f'Оценка выборочного среднеквадратического отклонения: {round(x.sample_standard_deviation, precision)}')
+    print(f'Оценка выборочного с.к.о. (исправленная): {round(x.sample_standard_deviation_corrected, precision)}')
     
-    plot_cdf(x, "output/task1/")
-    plot_polygon(x, "output/task1/")
-    plot_cumulative(x, "output/task1/")
+    for i in range(1, 4 + 1):
+        print(f"Центральный момент {i} порядка:{round(x.get_nth_moment(i), precision)}")
+
+    print(f"Мода: {x.mode}")
+    print(f"Медиана: {x.get_median()}")
+
+    print(f"Коэффициент ассиметрии: {round(x.get_nth_moment(3), precision)}")
+    print(f"Эксцесса: {round(x.get_nth_moment(4) - 3, precision)}")
+   
     
     
     
