5a0a643a47e077b10eb0f266a6bf9ef310ac24fd
nerfthisdev <353054@niuitmo.ru>
2025-04-09 14:32:19 +0300

    change parameters, add cumulative function

--- src/stats/plotting.py
+++ src/stats/plotting.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 
+from stats.variation import Variation
 
-def plot_cdf(nums: list[float], func, dir_path: str):
+
+def plot_cdf(variation_series: Variation, dir_path: str):
+    nums = variation_series.data
+    func = lambda k: variation_series.get_cdf(k)
     x_values = nums.copy()
     x_values.append(min(nums) - 0.5)
     x_values.append(max(nums) + 0.5)
@@ -19,7 +23,9 @@ def plot_cdf(nums: list[float], func, dir_path: str):
 
 
 
-def plot_polygon(x_vals: list[float], y_vals: list[float], dir_name: str):
+def plot_polygon(variation_series:Variation, dir_name: str):
+    x_vals = list(variation_series.statistical_series.keys())
+    y_vals = list(variation_series.statistical_series.values())
     if len(x_vals) != len(y_vals):
         print("ERROR: x_vals: list[float] != y_vals: list[float]")
         return
@@ -31,3 +37,18 @@ def plot_polygon(x_vals: list[float], y_vals: list[float], dir_name: str):
     plt.savefig(dir_name + "polygon.pdf")
     plt.savefig(dir_name + "polygon.png")
     plt.close()
+
+def plot_cumulative(variation_series:Variation, dir_name: str): 
+    x_vals = list(variation_series.statistical_series.keys())
+    y_vals = variation_series.cumulative_values
+    if len(x_vals) != len(y_vals):
+        print("ERROR: x_vals: list[float] != y_vals: list[float]")
+        return
+
+    plt.plot(x_vals, y_vals, marker="o", color="blue", label="Полигон")
+
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(dir_name + "cumulative.pdf")
+    plt.savefig(dir_name + "cumulative.png")
+    plt.close() 
